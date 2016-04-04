0524012a3a21577e922f1056504b2ece880d1be0
Alex Fallenstedt and Josh Gustafson <a.fallenstedt@gmail.com>
2016-04-04 13:07:01 -0700

    remove promise from index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return Ember.RSVP.hash({
-      category: this.store.findAll('category'),
-      listing: this.store.findAll('listing')
-    });
+    return this.store.findAll('category');
+
   }
 });
